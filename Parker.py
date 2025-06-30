5c5fd50e322d98c7d84c65e57c308ed211c6aa16
Dominic Payne <dspayne@csrlap2024-0091.local>
2025-06-30 17:33:09 -0400

    Rudimentary Sr & labelling

--- Parker.py
+++ Parker.py
@@ -69,6 +69,10 @@ def get_ppar(n,T,B):  #B ant T tensor coord systems need to match for this
 	P_par = n*T_par
 	return P_par
 
+def get_vxb(v,B):
+	vxb = np.cross(v,B)
+	return vxb
+
 
 
 def get_Tcomps(Ttensor,B):
@@ -78,16 +82,19 @@ def get_Tcomps(Ttensor,B):
 # %%
 # Get Mag Data
 
-trange = ['2024-10-29/00:00', '2024-10-31/00:00']
+# trange = ['2024-10-29/00:00', '2024-10-31/00:00']
 # trange = ['2018-11-1', '2018-11-10'] #Dudok de wit 2020 Full interval
-trange = ['2022-02-25', '2022-02-28'] #Dudok de wit 2020 Full interval
+#trange = ['2021-04-28', '2021-04-30'] # Encounter 8 (some sub-Alfvenic)
+trange = ['2021-08-09/12:00', '2021-08-10/00:00'] # Encounter 9 (some sub-Alfvenic)
+#trange = ['2022-02-25', '2022-02-28'] #Dudok de wit 2020 Full interval
+#trange = ['2018-11-05/00:00', '2018-11-05/03:00'] # Bale 2019 event (includes Sr)
 #trange = ['2021-08-11/09:00', '2021-08-12/09:00'] # Soni 2024 Parker interval
 
 Bfld_vars = pyspedas.projects.psp.fields(trange=trange, level='l2', time_clip=True)
 ACfld_vars = pyspedas.projects.psp.fields(trange=trange, datatype='dfb_ac_spec', level='l2',time_clip=True)
 DCfld_vars = pyspedas.projects.psp.fields(trange=trange, datatype='dfb_dc_spec', level='l2',time_clip=True)
 
-spc_vars = pyspedas.projects.psp.spc(trange=trange, datatype='l2', level='l2')
+# spc_vars = pyspedas.projects.psp.spc(trange=trange, datatype='l2', level='l2')
 swp_vars = pyspedas.projects.psp.spi(trange=trange,level='l3',time_clip=True)
 
 #%%1G
@@ -99,7 +106,7 @@ TiTensor_name = 'psp_spi_T_TENSOR_INST'
 Ti_name = 'psp_spi_TEMP'
 ni_name = 'psp_spi_DENS'
 
-interpvar_name = ni_name
+interpvar_name = vi_name
 timeax = pytplot.get_data(interpvar_name).times
 
 tinterpol(B_name,interpvar_name,newname='B')
@@ -116,27 +123,29 @@ ni = 1e6*reform(pytplot.get_data('ni'))
 Ti = 1.602e-19*reform(pytplot.get_data('Ti'))
 TiTensor = 1.602e-19*reform(pytplot.get_data('TiTensor'))
 
-
-
 # %%
 # Calculate B Magnitude & create Normalized Br/|B|
 Br_norm = np.zeros_like(Bvecs[:,0])
 vr_norm = np.zeros_like(Bvecs[:,0])
+E_conv = np.zeros_like(Bvecs)
+ExB = np.zeros_like(Bvecs)
 P_mag = np.zeros_like(Br_norm)
 P_th = np.zeros_like(ni)
 va = np.zeros_like(Br_norm)
 vth = np.zeros_like(Br_norm)
 vs = np.zeros_like(Br_norm)
-beta = np.zeros_like(ni)
 PiTensor = np.zeros_like(TiTensor)
 viandva = np.zeros([len(timeax),2])
 v_ratio = np.zeros([len(timeax),2])
+beta = np.zeros([len(timeax),2])
 pressures = np.zeros([len(timeax),3])
 line = np.ones_like(ni)
 theta = np.zeros([len(timeax),2])
 theta_v = np.zeros([len(timeax),2])
 
 for i in range(len(Bvecs)):
+	E_conv[i] = -get_vxb(vivecs[i],Bvecs[i])
+	ExB[i] = get_vxb(E_conv[i],Bvecs[i]) # Will probably need to revisit under better assumptions
 	Br_norm[i] = get_brnorm(Bvecs[i])
 	vr_norm[i] = get_brnorm(vivecs[i])
 	P_mag[i] = get_pm(Bvecs[i])
@@ -144,7 +153,7 @@ for i in range(len(Bvecs)):
 	va[i] = get_va(Bvecs[i],ni[i],mi)
 	vs[i] = get_vs(Ti[i],mi)
 	vth[i]  = get_vth(Ti[i],mi)
-	beta[i] = P_th[i]/P_mag[i]
+	beta[i] = [P_th[i]/P_mag[i],1]
 	v_ratio[i] = [np.linalg.norm(vivecs[i])/va[i],1]
 	PiTensor[i] = ni[i]*TiTensor[i]
 	viandva[i] = [np.linalg.norm(vivecs[i]),va[i]]
@@ -158,32 +167,90 @@ plt.legend()
 # plt.axhline(y=1,linestyle='--',color='k')
 # %%
 # Make it a Tplot variable & plot it
-store_data('beta', data = {'x':timeax,'y':beta})
+
+store_data('E_conv', data = {'x':timeax,'y':E_conv})
+
+#--------------------------------------------------------------------------------------
+# Radial Poynting Flux.  This is still shaky, as it assumes E = -vxB then uses that E for ExB
+# In principle, E x (-vxB) = 0 by definition, so idk why it becomes a non-zero signal
+# Better versions of this should use direct Efield data or maybe use background parker spiral for the ExB
+# but not for the calculation of -vxB
+store_data('Sr', data = {'x':timeax,'y':ExB[:,0]/mu0})
+store_data('Sr_norm', data = {'x':timeax,'y':ExB[:,0]/(P_mag*mu0)})
+#---------------------------------------------------------------------------------------
+
+
+
+# ------Angles---------------------------------
 store_data('theta', data = {'x':timeax,'y':theta})
 store_data('theta_v', data = {'x':timeax,'y':theta_v})
+#----------------------------------------------
+
+
+
+# ----Pressures-------------------------------
+store_data('Pt', data = {'x':timeax,'y':P_th})
+store_data('Pm', data = {'x':timeax,'y':P_mag})
+store_data('pressures', data = {'x':timeax,'y':pressures})
+#---------------------------------------------
+
+
+
+## -----Various Dimensionless Parameters -------
+# Alfvenic Mach Number (Ma)
+store_data('Ma', data = {'x':timeax,'y':v_ratio})
+
+# Plasma (proton) beta
+store_data('beta', data = {'x':timeax,'y':beta})
+
+# Br/|B|
+store_data('Br_norm', data = {'x':timeax,'y':Br_norm})
+
+# vr/|v|
+store_data('vr_norm', data = {'x':timeax,'y':vr_norm})
+
+
+
+store_data('vs_ratio', data = {'x':timeax,'y':vs/va})
+
+#-----------------------------------------------
+
+
+
+# ----Ion moments-----
 store_data('vi', data = {'x':timeax,'y':vivecs})
 store_data('n', data = {'x':timeax,'y':ni})
 store_data('T', data = {'x':timeax,'y':Ti})
-store_data('Pm', data = {'x':timeax,'y':P_mag})
-store_data('Pt', data = {'x':timeax,'y':P_th})
-store_data('pressures', data = {'x':timeax,'y':pressures})
-store_data('v_ratio', data = {'x':timeax,'y':v_ratio})
-store_data('vs_ratio', data = {'x':timeax,'y':vs/va})
+#----------------------
+
+
+# Both Velocities on one plot
 store_data('viandva', data = {'x':timeax,'y':viandva})
-store_data('Br_norm', data = {'x':timeax,'y':Br_norm})
-store_data('vr_norm', data = {'x':timeax,'y':vr_norm})
+
+
 pyspedas.ylim('v_ratio',0,5)
-pyspedas.options('v_ratio', 'ytitle', 'Vp/Va')
-pyspedas.options('v_ratio', 'linestyle', ['-','--'])
-pyspedas.options('v_ratio', 'color', 'k')
-pyspedas.tsmooth('v_ratio',0) # creates a 'v_ratio-s' variable
+pyspedas.options('Ma', 'ytitle', 'Vp/Va')
+
+pyspedas.options('Ma', 'ylog', 1)
+pyspedas.options('beta', 'ylog', 1)
+
+pyspedas.options('Ma', 'linestyle', ['-','--'])
+pyspedas.options('Ma', 'color', 'k')
+pyspedas.ylim('Ma', 0.3,3)
+
+pyspedas.options('beta', 'linestyle', ['-','--'])
+pyspedas.options('beta', 'color', 'k')
+pyspedas.ylim('beta', 0.1,10)
+
+pyspedas.tsmooth('Ma',0) # creates a 'Ma-s' variable
 pyspedas.ylim('theta',0,180)
-pyspedas.ylim('beta',0,2)
 pyspedas.options('theta', 'ytitle', 'Deflection Angle (degrees)')
 pyspedas.options('theta', 'linestyle', ['-','--'])
 pyspedas.options('theta', 'color', 'k')
 pyspedas.tsmooth('v_ratio',0) # creates a 'v_ratio-s' variable
-tplot(['Br_norm','theta','theta_v','beta',B_name])
+
+
+tplot(['Sr','Sr_norm','P_mag'])
 
 
 # %%
